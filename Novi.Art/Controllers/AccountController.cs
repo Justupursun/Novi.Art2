c7cece7d337105ff0667a6a6d6c0c14f3d47ebb2
Justupursun <martijnterstappen@hotmail.com>
2020-09-05 00:34:54 +0200

    - Add authentication to the web.config - Add logincontroller and view - Add accountcontroller and view

---
+++ Novi.Art/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Novi.Art.Controllers
+{
+    [Authorize] // Check for a valite user to allow access to all of the actions in this controller
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Novi.Art/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Novi.Art.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // create a valid login-cookie when login the loginform is summited
+        [HttpPost] // this will tell asp.NET MVC 5 to route any post requests to this index route to this specific method
+        public ActionResult Index(string username, string password)
+        {
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                FormsAuthentication.SetAuthCookie(username, false);                     // Using System.Web.Security
+                return Redirect(FormsAuthentication.GetRedirectUrl(username, false));
+            }
+            return View();
+        }
+    }
+}
