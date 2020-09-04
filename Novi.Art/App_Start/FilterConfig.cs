a16d14673386457df1fc7e531123e5d24dbce17a
Justupursun <martijnterstappen@hotmail.com>
2020-09-05 01:13:08 +0200

    Add LogRequestFilter-class Add the  LogRequestFilter-class globally to the filterConfig-class

--- Novi.Art/App_Start/FilterConfig.cs
+++ Novi.Art/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using Novi.Art.Filters;
+using System.Web;
 using System.Web.Mvc;
 
 namespace Novi.Art
@@ -8,6 +9,7 @@ namespace Novi.Art
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new LogRequestFilter());
         }
     }
 }
---
+++ Novi.Art/Filters/LogRequestFilter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Novi.Art.Filters
+{
+    public class LogRequestFilter
+    {
+    }
+}
