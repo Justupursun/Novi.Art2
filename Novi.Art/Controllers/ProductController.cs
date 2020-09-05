59bfdda0031f0d9cbbc58164b8ea07be7288b409
Justupursun <martijnterstappen@hotmail.com>
2020-09-06 01:42:28 +0200

    - Create Class Product - Improve navigation with data from the productcontroller

--- Novi.Art/Controllers/ProductController.cs
+++ Novi.Art/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Novi.Art.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,18 @@ namespace Novi.Art.Controllers
         [Route("product/{productName}")] //(Attribute routing)
         public ActionResult Detail(string productName)
         {
+
+            ViewBag.Product = new Product
+            {
+                Name = "lovecouple",
+                //                FullPrice = 20.00M,
+                CurrentPrice = 16.00M,
+                //                PercentOff = 20,
+                ImagePath = "~/Content/Images/Products/lovecouple.png",
+                StarRating = 4,
+                StartDate = DateTime.Now.Date,
+                EndDate = DateTime.Now.AddDays(60)
+            };
             //lookup the productname in the database
             return View();
         }
