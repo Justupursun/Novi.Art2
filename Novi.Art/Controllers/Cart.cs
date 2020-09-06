ebf85e6b8715442cd770c628e8f62aed5c214b9c
Justupursun <martijnterstappen@hotmail.com>
2020-09-06 20:15:12 +0200

    -Add Cartview - Add Cartclass - Add LayoutNoFooter

---
+++ Novi.Art/Controllers/Cart.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace Novi.Art.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
