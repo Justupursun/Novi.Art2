c7e4725dc0e1262194190a2d94adb57fa105e5a8
Justupursun <martijnterstappen@hotmail.com>
2020-09-10 11:59:12 +0200

    Add project files.

---
+++ Novi.Art/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Novi.Art.Startup))]
+namespace Novi.Art
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
